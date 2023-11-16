1f0017d81960ecc561ba057529aa9307613e2349
ficov <fv54188@fer.hr>
2023-11-16 11:29:03 +0100

    Dodan bazični izgled stanice sa mjerenjima po tjednima

--- aquaobserver/src/main.tsx
+++ aquaobserver/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import History from './components/history-page'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <History></History>
   </React.StrictMode>,
 )
