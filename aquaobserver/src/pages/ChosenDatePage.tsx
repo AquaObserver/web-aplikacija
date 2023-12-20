0295410529850a8b10237e95d6d48c15fce2207f
dominikpapes <dp54371@fer.hr>
2023-12-21 00:47:33 +0100

    promjena ruta u ChosenDatePage.tsx

--- aquaobserver/src/pages/ChosenDatePage.tsx
+++ aquaobserver/src/pages/ChosenDatePage.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import TimesChart from '../components/TimesChart';
-import './ChoseDatePage.css';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import TimesChart from "../components/TimesChart";
+import "./ChoseDatePage.css";
 
 export default function ChosenDatePage() {
   const params = useParams();
@@ -11,11 +11,11 @@ export default function ChosenDatePage() {
     labels: [],
     datasets: [
       {
-        label: 'Water Level',
+        label: "Water Level",
         data: [],
         fill: true,
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
         tension: 0.5,
       },
     ],
@@ -24,29 +24,29 @@ export default function ChosenDatePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8000/readings/${date}`);
+        const response = await fetch(`/api/readings/${date}`);
         if (!response.ok) {
-          throw new Error('Failed to fetch data for the selected date');
+          throw new Error("Failed to fetch data for the selected date");
         }
 
         const jsonData = await response.json();
         const data = jsonData.data; // Assuming the response has a "data" property
 
         setChartData({
-          labels: data.map((entry: { time: any; }) => entry.time),
+          labels: data.map((entry: { time: any }) => entry.time),
           datasets: [
             {
-              label: 'Razina vode',
-              data: data.map((entry: { waterLevel: any; }) => entry.waterLevel),
+              label: "Razina vode",
+              data: data.map((entry: { waterLevel: any }) => entry.waterLevel),
               fill: true,
-              borderColor: 'rgb(53, 162, 235)',
-              backgroundColor: 'rgba(53, 162, 235, 0.5)',
+              borderColor: "rgb(53, 162, 235)",
+              backgroundColor: "rgba(53, 162, 235, 0.5)",
               tension: 0.5,
             },
           ],
         });
       } catch (error) {
-        console.error('Error fetching data:', (error as Error).message);
+        console.error("Error fetching data:", (error as Error).message);
       }
     };
 
--- aquaobserver/vite.config.ts
+++ aquaobserver/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: LOCAL_API,
+        target: NGROK_API,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
